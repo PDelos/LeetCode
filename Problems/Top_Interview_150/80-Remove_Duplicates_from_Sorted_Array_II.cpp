340d32039390c618da574aa24f48b5a5771b919f
Pol de los Santos Subirats <132887066+PDelos@users.noreply.github.com>
2024-11-26 19:16:54 +0100

    Update 80-Remove_Duplicates_from_Sorted_Array_II.cpp

--- Problems/Top_Interview_150/80-Remove_Duplicates_from_Sorted_Array_II.cpp
+++ Problems/Top_Interview_150/80-Remove_Duplicates_from_Sorted_Array_II.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        vector<int>check(201, 0);
+        vector<int>check(2*1e4+1, 0);
         int count = 0;
         for(int& i:nums){
-            if(check[i+100] > 0){
-                i = 101;
+            if(check[i+1e4] > 0){
+                i = 1e4+1;
                 continue;
             }
-            check[i+100] += 1;
+            check[i+1e4] += 1;
             count += 1;
         }
 
