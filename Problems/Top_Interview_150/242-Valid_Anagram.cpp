1adf0639a8f1d4efbad5eafb369b1c3adfbac09f
Pol de los Santos Subirats <132887066+PDelos@users.noreply.github.com>
2024-12-03 21:50:54 +0100

    Create 242-Valid_Anagram.cpp

---
+++ Problems/Top_Interview_150/242-Valid_Anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        int count[26] = {0};
+        for(char ch : s) ++count[ch- 'a'];
+        for(char ch : t) {
+            --count[ch-'a'];
+            if(count[ch-'a'] < 0) return false;
+        }
+        return true;
+    }
+};
